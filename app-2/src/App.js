eb856d4449bd8682b8f04ceb99290e7a7cd59cff
Clifford Christensen <victoria.thursday@gmail.com>
2018-05-19 09:16:08 -0600

    finished app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { dogs: ['westie', 'wolfhound', 'bichon'] }
+  }
   render() {
+    let showDogs = this.state.dogs.map( (element, index) => {
+      return (
+        <h2 key ={ index }>{ element }</h2>
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h2>{ showDogs }</h2>
       </div>
     );
   }
