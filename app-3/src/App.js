e84f1990afb8dec714986e3c4119d28a97c94653
Clifford Christensen <victoria.thursday@gmail.com>
2018-05-19 10:37:31 -0600

    added case insensitivity to app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = { 
+      filterString: "",
+      dogs: ['Westie', 'wolfhound', 'bichon'] 
+    }
+   this.handleChange=this.handleChange.bind(this) 
+  }
+
+  handleChange( filter ) {
+    this.setState({ filterString: filter })
+  }
+
   render() {
+    let showDogs = this.state.dogs.filter((element, index, orgArr)=>{
+      return element.toLowerCase().includes(`${this.state.filterString.toLowerCase()}`)
+    }).map((element, index)=>{
+      return <h2 key={ index }>{ element }</h2>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input type="text" value={this.state.filterString} onChange={ (e) => this.handleChange( e.target.value ) }/>
+      {showDogs}
       </div>
     );
   }
